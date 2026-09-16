9d04f81bdb7df6e5037e73c5c801f7b1d7524b4b
madison <yyc930929@gmail.com>
2026-09-16 16:05:04 +0900

    내용 추가

--- 함수문제.py
+++ 함수문제.py
@@ -4,17 +4,18 @@
 # num = list(map(int, input("정수 입력:").split(",")))
 # print(f"{avg(num):.2f}")
 
+
 # 세자리 정수 중 큰 수 구하기
-def slice_num(num):
-    x = num // 100
-    y = (num % 100) // 10
-    z = num % 10
-    return (x, y, z)
-num = int(input("3자리 수 입력: "))
-x, y, z = slice_num(num)
-if x > y >= z or x > z >= y:
-    print(f"'{x}' 100의 자리 수가 가장 큽니다.")
-elif y > x >= z or y > z >= x:
-    print(f"'{y}' 10의 자리 수가 가장 큽니다.")
-else:
-    print(f"'{z}' 1의 자리 수가 가장 큽니다.")
+# def slice_num(num):
+#     x = num // 100
+#     y = (num % 100) // 10
+#     z = num % 10
+#     return (x, y, z)
+# num = int(input("3자리 수 입력: "))
+# x, y, z = slice_num(num)
+# if x > y >= z or x > z >= y:
+#     print(f"'{x}' 100의 자리 수가 가장 큽니다.")
+# elif y > x >= z or y > z >= x:
+#     print(f"'{y}' 10의 자리 수가 가장 큽니다.")
+# else:
+#     print(f"'{z}' 1의 자리 수가 가장 큽니다.")
