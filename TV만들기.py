f3f26e8f190dffecfdbf39f626f6929d52dee01e
madison <yyc930929@gmail.com>
2026-09-17 14:59:46 +0900

    내용 추가

---
+++ TV만들기.py
@@ -0,0 +1,36 @@
+class Television: #클래스 이름은 대문자로 시작
+    def __init__(self, name, on, channel, volume): # 생성자
+        self.name = name
+        self.on = on
+        self.channel = channel
+        self.volume = volume
+
+    def set_on(self, on):
+        self.on = on
+
+    def set_channel(self, channel):
+        self.channel = channel
+
+    def set_volume(self, volume):
+        self.volume = volume
+
+    def get_on(self):
+        return self.on
+
+    def get_channel(self):
+        return self.channel
+
+    def get_volume(self):
+        return self.volume
+
+    def view_tv(self):
+        power = ("OFF", "ON")
+        print(f"이름 : {self.name}")
+        print(f"전원 : {power[self.on]}")
+        print(f"채널 : {self.channel}")
+        print(f"볼륨 : {self.volume}")
+
+lg_tv = Television("LG", True, 10, 10)
+lg_tv.view_tv()
+samsung_tv = Television("Samsung", False, 20, 20)
+samsung_tv.view_tv()
