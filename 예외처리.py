2883ebeeb88c3812078e73f4f0ce6ccec2bcbda9
madison <yyc930929@gmail.com>
2026-09-18 16:03:25 +0900

    내용 추가

--- 예외처리.py
+++ 예외처리.py
@@ -2,3 +2,16 @@
 # 처리하여 프로그램이 비정상적으로 종료되지 않도록 하는 방법입니다.
 # 예외 처리는 주로 try, except, else, finally 블록을 사용하여 처리합니다.
 
+try:
+    print("나눗셈 계산기 입니다.")
+    num1 = int(input("첫 번째 숫자 입력: "))
+    num2 = int(input("두 번째 숫자 입력: "))
+    print(f"{num1} / {num2} = {int(num1/num2)}")
+except ValueError:
+    print("에러!!! 잘못된 값을 입력하였습니다.")
+except ZeroDivisionError as err:
+    print(err)
+else:
+    print("정상 처리되었습니다.")
+finally:
+    print("프로그램 실행 완료!!")
